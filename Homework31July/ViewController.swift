96a30f77a8c2c1703c0b6ad5dc30b6e63a32de43
Dominika <dominikapiganova@mail.ru>
2021-07-31 17:21:19 +0300

    Initial Commit

---
+++ Homework31July/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework31July
+//
+//  Created by Dominika Poleshyck on 31.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
